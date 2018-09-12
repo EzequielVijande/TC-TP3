ccf24748c8840b18e590dfd8287967e697403b3a
Ezequiel Vijande <evijande@itba.edu.ar>
2018-09-12 15:11:53 -0300

    arregle un error en el codigo

--- TP3/Ej1-GIC/Sim.m
+++ TP3/Ej1-GIC/Sim.m
@@ -1,32 +1,61 @@
 close all;
-R1=1e3;
-R3=1e3;
-R4=1e3;
-R5=100e3;
-R7=1e3;
-R8=1e3;
-R6=4e3;
+R=1636.66;
+C=47e-9;
+k=0.515;
+    Q=2;
+    R1=R;
+    R3=R;
+    R4=((2*(k^2))/(1+(k^2)) )*R;
+    R5=((2*(k^2))/(1-(k^2)) )*R;
+    R7=Q*(1+(1/(k^2)))*R;
+    R8=R;
+    R6=Q*(1+(k^2))*R;
 
-A=1000e9;
+    A=1000e9;
 
-C2=100e-9;
-C6=100e-9;
+    C2=C;
+    C6=C;
 
-Ra=R4*R8+R5*R8+R4*R5;
+    Ra=R4*R8+R5*R8+R4*R5;
+    Ru=R1*R6+R6*R7+R7*R1;
 
-s=tf('s');
-x1=((C2*R3*s)+1);
-x2=(R1+2*R6+C6*R6*R7*s);
-x3=Ra;
-
-k=( R6*(R4*R5*R7+(R1*Ra)/A))/(R7*((Ra*(R1+2*R6))/A+R6*Ra-R5*R6*R8+(Ra*(R1+2*R6))/(A^2)));
-w1=sqrt((R4*R5*R7+(R1*Ra)/A)/(C2*C6*R3*R7*(R1*Ra*(1+(1/A))-R4*R5*R7)));
-w2=sqrt( ( (Ra*(R1+2*R6))/A+R6*Ra-R5*R6*R8+(Ra*(R1+2*R6))/(A^2))/(C2*R3*R6*R7*(Ra/A+C6*R5*R8+(C6*Ra)/(A^2))) );
-q1= (R6*sqrt( (R4*R5*R7+(R1*Ra)/A)*(C2*C6*R3*R7*(R1*Ra*(1+1/A)-R4*R5*R7))))/(C2*R3*R4*R5*R6*R7-(R1+2*R6)*C2*R3*R4*R5*R7+R1*R6*Ra*((C6*R7+C2*R3)/A+C2*R3));
-q2= ( sqrt( ((Ra*(R1+2*R6))/A+R6*Ra-R5*R6*R8+(Ra/(A^2))*(R1+2*R6) )*(C2*R3*R6*R7*(Ra/A+C6*R5*R8+(C6*Ra)/A))) )/( (C6*R6*R7*Ra+C2*R3*Ra*(R1+2*R6))/A+(R1+R6)*C2*R3*R5*R8+(Ra/(A^2))*(C2*R1*R3+2*C2*R3*R6+C6*R6*R7)) ;
+    s=tf('s');
 
 
-num= ( (s/w1)^2 )+(s/(w1*q1))+1;
-den= ( (s/w2)^2 )+(s/(w2*q2))+1;
-func=k*(num/den);
-bode(func)
+    num1=C2*C6*R1*R3*R6*R7*(Ra*(1+1/A)-R4*R5);
+    num2=(R1*R6*Ra*((C6*R7)/A+C2*R3*(1+1/A))+C2*R3*R4*R5*R6*R7-C2*C6*R1*R3*R4*R5*R6*R7-C2*R3*R4*R5*Ru);
+    num3=R6*((R1*Ra)/A+R4*R5*R7);
+
+    den1=C2*C6*R1*R3*R6*R7*((Ra/A)*(1+1/A)+R5*R8);
+    den2=( (C6*R1*R6*R7*Ra+C2*R3*Ra*Ru)/A-C2*R3*R5*R6*R7*R8+C2*R3*R5*R8*Ru+(Ra/(A^2))*(C2*R3*Ru+C6*R1*R6*R7));
+    den3=(Ra*Ru)/A+R6*R7*Ra-R5*R6*R7*R8+(Ru*Ra)/(A^2);
+
+    num=num1*(s^2)+...
+        num2*s+...
+        num3;
+    den= den1*(s^2)+...
+        den2*s+...
+        den3;
+
+    func=(num/den);
+    wx=((1*2*pi):(20*pi):((1e5)*(2*pi)));
+    [mag, fase]= bode(func, wx);
+    mag2=squeeze(mag);
+    fase2=squeeze(fase);
+    magDB= 20*log10(mag2);
+    f=wx./(2*pi);
+    figure(1)
+    semilogx(f,magDB);
+    hold on
+    title('Bode de magnitud');
+    xlabel('f(Hz)')
+    ylabel('Mag(DB)')
+    grid on
+    figure(2)
+    semilogx(f,fase2);
+    hold on
+    title('Bode de fase');
+    xlabel('f(Hz)')
+    ylabel('fase(deg)')
+    grid on
+
--- TP3/Ej1-GIC/SimValoresHpNotch.m
+++ TP3/Ej1-GIC/SimValoresHpNotch.m
@@ -5,52 +5,28 @@ s=sym('s');
 %Resistencias
 R1=sym('R1');
 R3=sym('R3');
-R4=sym('R4');
+R8=sym('R8');
+R4= sym('R4');
 R5=sym('R5');
 R6=sym('R6');
 R7=sym('R7');
-R8=sym('R8');
 
 %Capacitores
 C2=sym('C2');
 C6=sym('C6');
 
 A=sym('A');
-wo=sym('wo');
+
 
 Avol=A;
-
 z6 = R6;
 z7 = R7/((s*C6*R7)+1);
 z2=1/(s*C2);
 Ra=R4*R8+R5*R8+R4*R5;
 Rb=R6*z7+R1*z7+R6*R1;
-
-%Ganancias de caminos directos
-G1=( (R4*R5)/Ra )*Avol*( R3/(R3+z2) )*(-Avol);
-G2= ( (R1*R6)/Rb )*Avol;
-G3= Avol*Avol*( (z7*R6)/Rb )*( (R4*R5)/Ra );
-
-%Ganancias de los loops
-loop1=(Avol)*( R3/(R3+z2))*(-Avol)*((R5*R8)/(Ra));
-loop2=Avol*( (R6*z7)/Rb )*Avol*( (R5*R8)/Ra);
-loop3=(-Avol)*( R3/(R3+z2));
-loop4=( (z7*R6)/Rb )*Avol*(-Avol)*(z2/(z2+R3));
-loop5=(-Avol)*( z2/(R3+z2));
-
-delta=1-(loop1+loop2+loop3+loop4+loop5);
-delta1=1;
-delta2=1-loop3;
-delta3=1;
-
-
-H=(G1*delta1+G2*delta2+G3*delta3)/delta;
-H=simplify(H,'IgnoreAnalyticConstraints',true);
-pretty(H)
-
 Ru=R1*R6+R6*R7+R7*R1;
 
-clc;
+
 num1=C2*C6*R1*R3*R6*R7*(Ra*(1+1/A)-R4*R5);
 num2=(R1*R6*Ra*((C6*R7)/A+C2*R3*(1+1/A))+C2*R3*R4*R5*R6*R7-C2*C6*R1*R3*R4*R5*R6*R7-C2*R3*R4*R5*Ru);
 num3=R6*((R1*Ra)/A+R4*R5*R7);
@@ -65,10 +41,23 @@ num=num1*(s^2)+...
 den= den1*(s^2)+...
     den2*s+...
     den3;
-wp= sqrt( 1/(den1/den3) );
-Q= 1/((den2/den3)*wp);
+%calculo parametros y aplico condiciones para resolver.
+%wp= sqrt( 1/(den1/den3) );
 wz= sqrt( 1/(num1/num3) );
-pretty(simplify(wz))
-%H=k*(num/den);
-%H=simplify(H,'IgnoreAnalyticConstraints',true);
-%pretty(H)
+k=(wz/wp);
+Q= 1/((den2/den3)*wp);
+
+
+%Ecuaciones
+wp=sym('wp');
+eq2= wp== sqrt( 1/(den1/den3) );
+eq3= R3==R1;
+eq4= R8==R1;
+eq5= R4== ((2*(k^2))/(1+(k^2)))*R1;
+eq6= R5==((2*(k^2))/(1-(k^2)))*R1;
+eq7= R6==(1+(k^2))*Q*R1;
+eq8= R7==(1+(1/(k^2)))*Q*R1;
+eq9= C2==C6;
+eqns=[eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9];
+solnum= solve(num==0, s,'IgnoreAnalyticConstraints', true);
+pretty(simplify(solnum))
